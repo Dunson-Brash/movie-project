168f40f37acbbfd8ecfd402929e899b5bda389b5
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-05-08 17:59:04 -0400

    added js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+let markUp =''
+fetch('https://puffy-easy-circle.glitch.me/movies')
+    .then(movieData => {return movieData.json()})
+    .then(data => {
+        console.log(data);
+        data.map( data => {
+            const movieTitle = data.title
+            const movieDirector = data.director
+            const ratings = data.rating
+            const genre = data.genre
+            const idNum = data.id
+
+            markUp += `
+            
+            `
+        })
+    })
