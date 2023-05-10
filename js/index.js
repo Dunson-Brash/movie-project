33b0448cd0945abdf0bc62e1babced3a81fc3e62
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-05-10 18:38:35 -0400

    added edit function

--- js/index.js
+++ js/index.js
@@ -20,23 +20,66 @@
                     const genre = data.genre
                     const idNum = data.id
 
-
                     markUp += `
-    <tr>
+    <tr class="editMovie">
       <td>${movieTitle}</td>
       <td>${movieDirector}</td>
       <td>${ratings}</td>
-      <td>${genre}</td>
-      <td><button id="edit" class="edit">Edit</button></td>
+      <td class="text-capitalize">${genre}</td>
+      <td><button value="${idNum}" class="edit">Edit</button></td>
     </tr>
+    <tr class="d-none">
+    
+    <form id="edit-form">
+      <td><input type="text" id="edit-title-input${idNum}" placeholder="${movieTitle}"></td>
+        <td><input type="text" id="edit-director-input" placeholder="${movieDirector}"></td>
+        <td>
+            <select id="edit-stars-input" required>
+                <option disabled selected>Rating</option>
+                <option>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            </td>
+            <td> <input class="text-capitalize" type="text" id="edit-genre-input" placeholder="${genre}"></td>
+        <td><button class="edit-input" id="edit-input" type="button">+</button></td> 
+        </form>
+</tr>
     
   `
                     markUp2 += `<option value="${idNum}" >${movieTitle}</option>`
 
+
                 })
                 $('.movie-container').html(markUp)
                 $('#delete-input').html(markUp2)
+
+
+                //      handle
             })
+            .then(() => {
+                $('.movie-container').on('click', '.edit', function () {
+                    let hiddenEdit = $(this).parent().parent().next()
+                    $(this).addClass('d-none')
+                    $(hiddenEdit).removeClass('d-none')
+
+                    let movieEditId = $(this).attr('value');
+                    return movieEditId
+
+                })
+            }).then(()=>{
+                $('.movie-container').on('click', `.edit-input`, function () {
+                    let editMovie = {
+                        title: $(this).parent().parent().children().children().first().val(),
+                        director: $(this).parent().parent().children().next().next().children().val(),
+                        rating: $(this).parent().parent().children().next().next().next().children().val(),
+                        genre: $(this).parent().parent().children().next().next().next().next().children().val(),
+                    }
+            })
+        })
     }
 
     renderMovies()
@@ -58,8 +101,7 @@
             },
             body: JSON.stringify(newMovie),
         };
-        fetch(url, options).then(res => console.log(res))
-        renderMovies()
+        fetch(url, options).then(res => console.log(res)).then(() => renderMovies())
 
     })
 
@@ -71,13 +113,31 @@
                 'Content-Type': 'application/json',
             },
 
-    }
-    fetch(`https://puffy-easy-circle.glitch.me/movies/${deleteItem}`, options).then(res => console.log(res))
-        renderMovies()
-    })
-
-    $('.movie-container').on('click','#edit',() =>{
+        }
+        fetch(`https://puffy-easy-circle.glitch.me/movies/${deleteItem}`, options).then(res => console.log(res)).then(() => {
+            renderMovies()
+        })
 
     })
+
+    //  HANDLE EDIT
+    //handleEdit(id){
+    // grab user input, create an body object with new info
+
+//     fetch -> PUT url/{id}
+//     body: userInput
+// }
+
+    // $('.movie-container').on('click',`#edit`,() => {
+    //     console.log($(this).attr("value"))
+    //     //handleEdit(id)
+    // })
+
+    let loader = $('#preloader');
+
+    $(loader).on('load', () => {
+
+    })
+
 })()
 
